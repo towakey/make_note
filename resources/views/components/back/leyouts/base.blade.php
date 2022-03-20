475ba5023ac59137e75419bdc9df95e23a03a835
towakey <kyosgame@gmail.com>
2022-03-20 13:05:52 +0900

    フラッシュメッセージのコンポーネント作成

---
+++ resources/views/components/back/leyouts/base.blade.php
@@ -0,0 +1,12 @@
+<main class="py-4">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-12">
+                <x-back.alert />
+                <div class="card">
+                    @yield('content')
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
