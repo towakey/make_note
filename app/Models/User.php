af946a2bf9f34fee45187f58fc9cba462c2700f3
towakey <kyosgame@gmail.com>
2022-03-26 20:05:25 +0900

    ユーザーパスワードのHASH化

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Hash;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
@@ -21,6 +22,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'role',
     ];
 
     /**
@@ -40,7 +42,19 @@ class User extends Authenticatable
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
+        'role' => 'integer',
     ];
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        self::saving(function($user){
+            if($user->password){
+                $user->password = Hash::make($user->password);
+            }
+        });
+    }
 
     public function notes()
     {
