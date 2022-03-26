26a2e7f3a92dd008c999ed67ec55b43e3004842c
towakey <kyosgame@gmail.com>
2022-03-26 19:39:31 +0900

    ユーザー管理画面を管理者のみが開けるように変更

--- routes/back.php
+++ routes/back.php
@@ -4,3 +4,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', 'DashboardController')->name('dashboard');
 
 Route::resource('notes', 'NoteController')->except('show');
+
+Route::group(['middleware' => 'can:admin'], function(){
+    Route::resource('users', 'UserController')->except('show');
+});
