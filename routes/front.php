ea8b9d77ce7f99e3e97f3399e28452c55d44deb2
towakey <kyosgame@gmail.com>
2022-03-27 12:37:22 +0900

    routeのタグ機能追加

--- routes/front.php
+++ routes/front.php
@@ -7,3 +7,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'NoteController@index')->name('home');
 Route::resource('notes', 'NoteController')->only(['index', 'show']);
+
+Route::get('notes/tag/{tagSlug}', 'NoteController@index')->where('tagSlug', '[a-z]+')->name('notes.index.tag');
